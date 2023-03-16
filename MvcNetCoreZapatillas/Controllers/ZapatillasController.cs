50545b855a49f1661eda149cc68e3c97eadbc3ba
SergioGuijarrodeCabo <sguijarrodcabo@gmail.com>
2023-03-16 14:34:00 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Controllers/ZapatillasController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreZapatillas.Models;
+using MvcNetCoreZapatillas.Repositories;
+
+namespace MvcNetCoreZapatillas.Controllers
+{
+    public class ZapatillasController : Controller
+    {
+
+        public RepositoryZapatillas repo;
+
+
+        public ZapatillasController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Zapatilla> zapatillas =  this.repo.GetZapatillas();
+            return View(zapatillas);
+        }
+
+        //public IActionResult _ZapatillaPartial(int idProducto)
+        //{
+        //   Zapatilla zapatilla = this.repo.FindZapatilla(idProducto);
+        //    return PartialView("_ZapatillaPartial", zapatilla);
+        //}
+
+        public IActionResult DetailsZapatilla(int idproducto)
+        {
+            Zapatilla zapatilla = this.repo.FindZapatilla(idproducto);
+            return View(zapatilla);
+        }
+
+        //public IActionResult ImagenesPaginadas(int idProducto)
+        //{
+        //    ViewData["TOTAL"] = this.repo.TotalZapatillas(idProducto);
+        //    return View();
+        //}
+
+
+
+        //public IActionResult ImagenesPaginadas(int idProducto, int registrospagina, int? posicion)
+        //{
+        //    if (posicion == null)
+        //    {
+        //        posicion = 1;
+        //    }
+
+        //    ViewData["TOTAL"] = this.repo.TotalZapatillas(idProducto);
+        //    ViewData["REGISTROPAGINA"] = registrospagina;
+
+        //    ImagenZapatilla imagen = this.repo.ImagenZapatilla(registrospagina, posicion.Value, idProducto);
+        //    return View(imagen);
+
+        //}
+
+        [HttpGet]
+        public IActionResult ImagenesPaginadas(int idProducto)
+        {
+
+            
+            ViewData["TOTAL"] = this.repo.TotalZapatillas(idProducto);
+            ViewData["REGISTROPAGINA"] = 10;
+
+            ImagenZapatilla imagen = this.repo.ImagenZapatilla(10, 1, idProducto);
+            return View(imagen);
+        }
+
+        [HttpPost]
+        public IActionResult ImagenesPaginadas(int idProducto, int? registrospagina, int? posicion )
+        {
+
+            if (registrospagina == null)
+            {
+
+                registrospagina = 10;
+            }
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            ViewData["TOTAL"] = this.repo.TotalZapatillas(idProducto);
+            ViewData["REGISTROPAGINA"] = registrospagina;
+
+            ImagenZapatilla imagen = this.repo.ImagenZapatilla(registrospagina.Value, posicion.Value, idProducto);
+            return View(imagen);
+        }
+
+
+
+
+    }
+}
---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+
+#region SQL SERVER
+//VUESTRO PROCEDIMIENTO DE PAGINACION DE IMAGENES DE ZAPATILLAS
+
+
+//CREATE OR ALTER PROCEDURE SP_GET_ZAPATILLAS(@REGISTROSPAGINA INT, @POSICION INT, @IDZAPATILLA INT)
+//AS
+//	SELECT * FROM (SELECT CAST(ROW_NUMBER() OVER(ORDER BY IDIMAGEN) AS INT) AS POSICION, IDIMAGEN, IMAGEN, IDPRODUCTO FROM IMAGENESZAPASPRACTICA WHERE IDPRODUCTO =@IDZAPATILLA) AS QUERY
+//	WHERE QUERY.POSICION >= @POSICION AND QUERY.POSICION < (@POSICION + @REGISTROSPAGINA) 
+//GO
+
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+
+
+        public List<Zapatilla> GetZapatillas()
+        {
+            var consulta = this.context.Zapatillas.ToList();
+            return consulta;
+
+
+        }
+        public Zapatilla FindZapatilla(int idProducto)
+        {
+            var consulta = this.context.Zapatillas.FirstOrDefault(x => x.IdProducto == idProducto);
+            return consulta;
+
+
+        }
+
+        public int TotalZapatillas(int idProducto)
+        {
+            return this.context.ImagenesZapatillas.Count(x => x.IdProducto ==idProducto);
+
+        }
+
+        public ImagenZapatilla ImagenZapatilla(int registrospagina, int posicion, int idProducto)
+        {
+            string sql = "SP_GET_ZAPATILLAS @REGISTROSPAGINA, @POSICION, @IDZAPATILLA";
+
+            SqlParameter pamReg = new SqlParameter("@REGISTROSPAGINA", registrospagina);
+            SqlParameter pamPos = new SqlParameter("@POSICION", posicion);
+            SqlParameter pamId = new SqlParameter("@IDZAPATILLA", idProducto);
+            var consulta = this.context.ImagenesZapatillas.FromSqlRaw(sql, pamReg, pamPos, pamId);
+
+            ImagenZapatilla imagen = consulta.AsEnumerable().FirstOrDefault();
+
+            return imagen;
+        }
+
+    }
+}
